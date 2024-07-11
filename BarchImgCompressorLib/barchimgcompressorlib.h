891e0c3dbf750346bd5754ec42b994d4f10912dc
Volodymyr Frytskyy <fritskiy@gmail.com>
2024-07-11 09:14:16 -0400

    Some further improvement

--- BarchImgCompressorLib/barchimgcompressorlib.h
+++ BarchImgCompressorLib/barchimgcompressorlib.h
@@ -6,6 +6,7 @@
 #include <vector>
 #include <cstdint>
 
+extern "C" {
 namespace BarchImgCompressorDecompressor {
 
     // Enum for compression result
@@ -23,17 +24,17 @@ namespace BarchImgCompressorDecompressor {
         unsigned char* data;    // compressed pixels
     };
 
-    eImgCompressionResult encodeBarchBitmap(const std::string& inputBMPFilePath, const std::string& outBarchFilePath);
-    eImgCompressionResult decodeBarchBitmap(const std::string& inputBarchFilePath, const std::string& outBMPFilePath);
+    Q_DECL_EXPORT eImgCompressionResult encodeBarchBitmap(const std::string& inputBMPFilePath, const std::string& outBarchFilePath);
+    Q_DECL_EXPORT eImgCompressionResult decodeBarchBitmap(const std::string& inputBarchFilePath, const std::string& outBMPFilePath);
 
     // Internal functions - for Jedi eyes only [but still, can be used by Sith Lords as they know what they do]
-    eImgCompressionResult readBMPFile(const std::string& filePath, Bitmap*& bitmap);
-    eImgCompressionResult writeBMPFile(const std::string& filePath, const Bitmap* bitmap);
-    std::vector<uint8_t> encodeRebelTransmission(const Bitmap* bitmap);
-    Bitmap* decodeImperialIntercept(const std::vector<uint8_t>& encodedData);
-    void freeBitmapMemory(Bitmap* bitmap);
+    Q_DECL_EXPORT eImgCompressionResult readBMPFile(const std::string& filePath, Bitmap*& bitmap);
+    Q_DECL_EXPORT eImgCompressionResult writeBMPFile(const std::string& filePath, const Bitmap* bitmap);
+    Q_DECL_EXPORT std::vector<uint8_t> encodeRebelTransmission(const Bitmap* bitmap);
+    Q_DECL_EXPORT Bitmap* decodeImperialIntercept(const std::vector<uint8_t>& encodedData);
+    Q_DECL_EXPORT void freeBitmapMemory(Bitmap* bitmap);
 
 } //end of namespace BarchImgCompressorDecompressor
-
+}
 
 #endif // BARCHIMGCOMPRESSORLIB_H
